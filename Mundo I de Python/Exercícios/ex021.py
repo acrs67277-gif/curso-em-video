99e07978e85ec382f03d177f00f026e8fcd4ea81
acrs67277-gif <acrs67277@gmail.com>
2026-06-24 14:53:04 -0400

    Atualização

--- Mundo I de Python/Exercícios/ex021.py
+++ Mundo I de Python/Exercícios/ex021.py
@@ -1,2 +1,6 @@
-from playsound import playsound 
-playsound('ex021.mp3')
+from playsound import playsound
+
+playsound('m-1.mp3','m-2.mp3')
+#playsound('m-1.mp3')
+#playsound('m-2.mp3')
+playsound('m-3.mp3')
