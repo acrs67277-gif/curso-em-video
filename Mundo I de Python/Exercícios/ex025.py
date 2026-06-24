9d08c3e95a9907496f8ce53429734da5f7a8714f
acrs67277-gif <acrs67277@gmail.com>
2026-06-24 15:19:57 -0400

    Update

--- Mundo I de Python/Exercícios/ex025.py
+++ Mundo I de Python/Exercícios/ex025.py
@@ -1,4 +1,4 @@
-nome = str(input('Qual é seu nome? ')).strip()
+nome = str(input('Qual é seu nome? ')).split()
 print('Seu nome tem Silva? {} '.format('SILVA' in nome.upper()))
 print('Seu nome tem Silva? {} '.format('silva' in nome.lower()))
 print('Seu nome tem Silva? {} '.format('Silva' in nome.capitalize()))
